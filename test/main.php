e4f5999fd7c425de6bab474f4fc1cbf27634ff78
roman-tu <65735484+roman-tu@users.noreply.github.com>
2020-05-26 21:56:31 +0600

    Add files via upload

---
+++ test/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+<?php
+echo '<div><img src="images/hKXuZDeHtSE.png" width="100px" height="100px"></div>?>';?>
+</body>
+</html>
+
