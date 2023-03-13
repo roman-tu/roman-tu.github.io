e3b33be22d3c47bc7b26ced6927f6c01f3670405
roman-tu <nudopustimroman@gmail.com>
2023-03-14 01:53:26 +0600

    last fix

--- main.js
+++ main.js
@@ -8,18 +8,20 @@ var app = new Vue({
 		idd: 0,
 		nameprod: 'Хлеб',
 		datetake: '03.03.23',
-		counts: '20',
+		counts: '0',
 		price: '0',
 		TotalSumm: 0
 	},
 	emits: ['update:value'],
 	methods: {
 		parseData: function () {
-			let jsonn = {id: this.component.length, nameProduct: this.nameprod, dateTake: this.datetake, counts: this.counts, price: this.price}
+			let jsonn = {id: this.component.length, nameProduct: this.nameprod, dateTake: this.datetake, counts: this.counts, price: this.price, totalsum: Number(this.price*this.counts) ,}
 			this.component.push(jsonn)
+			let ts
 			for(let i = 0; this.component.length > i; i++){
-				this.TotalSumm += Number(this.component[i].price)
+				ts = Number(this.component[i].totalsum)
 			}
+			this.TotalSumm += ts
 		}
 	  }
 })
